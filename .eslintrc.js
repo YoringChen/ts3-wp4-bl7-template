033374d2e076dc6ee6a394fb892b76ca01cbf3b0
cyh <cyh@hitour.cc>
2018-10-10 10:17:43 +0800

    feat(project): inited

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  'root': true,
+  'parser': 'typescript-eslint-parser',
+  'plugins': [
+    'typescript'
+  ],
+  'extends': 'eslint-config-standard-with-typescript',
+  'rules': {
+    'semi': 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? "on" : "off",
+  }
+}
